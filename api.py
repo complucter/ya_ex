dcfbe98165fe63df5fe053a1a87a2254dfaf158f
Emil <75830897+complucter@users.noreply.github.com>
2021-04-29 15:21:19 +0300

    #

--- api.py
+++ api.py
@@ -1,26 +1,23 @@
-# coding: utf-8
-# Импортирует поддержку UTF-8.
+
 from __future__ import unicode_literals
 
-# Импортируем модули для работы с JSON и логами.
+
 import json
 import logging
 import random
-# Импортируем подмодули Flask для запуска веб-сервиса.
+
 from flask import Flask, request
 
 app = Flask(__name__)
 
 logging.basicConfig(level=logging.DEBUG)
 
-# Хранилище данных о сессиях.
 sessionStorage = {}
 
 
-# Задаем параметры приложения Flask.
 @app.route("/", methods=['POST'])
 def main():
-    # Функция получает тело запроса и возвращает ответ.
+
     logging.info('Request: %r', request.json)
 
     response = {
